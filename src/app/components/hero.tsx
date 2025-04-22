e411027bdf2145ed3a2862134e37f73c815ca7b8
Pintu kumar <pintu1012kumar@gmail.com>
2025-04-23 01:34:03 +0530

    dummy blog added

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,92 +4,93 @@ import { HeroParallax } from "../components/ui/hero-parallax";
 
 const products = [
   {
-    title: "Moonbeam",
-    link: "https://gomoonbeam.com",
+    title: "I Stopped Using Kubernetes",
+    link: "https://medium.com/stackademic/i-stopped-using-kubernetes-our-devops-team-is-happier-than-ever-a5519f916ec0",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*t5dwn8IabnCdEpoO-6Toog.png",
   },
   {
-    title: "Cursor",
-    link: "https://cursor.so",
+    title: "System Design For Beginners: @shivambhadani_",
+    link: "https://medium.com/@shivambhadani_/system-design-for-beginners-everything-you-need-in-one-article-c74eb702540b",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*NZqp4-jhGa1ZY_-HX95reQ.png",
   },
   {
-    title: "Rogue",
-    link: "https://userogue.com",
+    title: "AWS",
+    link: "https://medium.com/@shivambhadani_/aws-part-1-introduction-to-cloud-and-ec2-f06cdc80a1fc",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*-j67_EIOJ7EE3qAoC3WqZA.png",
   },
   {
-    title: "Editorially",
-    link: "https://editorially.org",
+    title: "Zomato Interview Experience for SDE 1 : @shivambhadani_",
+    link: "https://medium.com/@shivambhadani_/zomato-interview-experience-for-sde-1-c0eed08e9d57",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Cq_JznDWgUC9tZT-5qpwrA.png",
   },
+
   {
     title: "Editrix AI",
-    link: "https://editrix.ai",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/editrix.png",
   },
   {
     title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
   },
   {
-    title: "Algochurn",
-    link: "https://algochurn.com",
+    title: "SQLite ",
+    link: "https://medium.com/@mendes.develop/quick-comparison-of-two-powerfull-frameworks-for-sqlite-when-creating-tables-active-record-and-af286d9c7906",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*EXCUSOPPN7A9i6NCEtLNtA.png",
   },
   {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
+    title: "Joining tables in MongoDB with Mongoose",
+    link: "https://medium.com/@mendes.develop/joining-tables-in-mongodb-with-mongoose-489d72c84b60",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*CZWfLjbKwAeZDiDoxx_IGw.png",
   },
   {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
+    title: "Introducing Hooks",
+    link: "https://medium.com/@mendes.develop/introducing-hooks-and-adding-state-to-function-components-6f8e291a348b",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/0*5cm9C1p2-PV2LL0D.png",
   },
   {
     title: "SmartBridge",
-    link: "https://smartbridgetech.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
   },
   {
     title: "Renderwork Studio",
-    link: "https://renderwork.studio",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
   },
   {
     title: "Creme Digital",
-    link: "https://cremedigital.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
   },
   {
     title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
   },
   {
     title: "Invoker Labs",
-    link: "https://invoker.lol",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/invoker.png",
   },
   {
     title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
   },
--- src/app/components/ui/hero-parallax.tsx
+++ src/app/components/ui/hero-parallax.tsx
@@ -136,15 +136,14 @@ export const ProductCard = ({
       key={product.title}
       className="group/product h-96 w-[30rem] relative shrink-0"
     >
-      <a href={product.link} className="block group-hover/product:shadow-2xl ">
-        <img
-          src={product.thumbnail}
-          height="600"
-          width="600"
-          className="object-cover object-left-top absolute h-full w-full inset-0"
-          alt={product.title}
-        />
-      </a>
+    <a href={product.link} className="block group-hover/product:shadow-2xl relative w-full h-96">
+  <img
+    src={product.thumbnail}
+    alt={product.title}
+    className="object-cover w-full h-full"
+  />
+</a>
+
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
